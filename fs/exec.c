621c0b1e1e6518856cfb95d01e0bbf239b320a62
John Vincent <git@tensevntysevn.cf>
2021-08-22 14:45:20 +0800

    kernel: Affine Samsung's HyPer HAL to the big CPU cluster
    While not a critical system service, it pretty much handles boosting on One UI so it might be ideal that this service runs on the faster CPU cluster.
    
    Signed-off-by: John Vincent <git@tensevntysevn.cf>

--- fs/exec.c
+++ fs/exec.c
@@ -97,6 +97,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
+#define SEM_HYPER_BIN_PREFIX "/vendor/bin/hw/vendor.samsung.hardware.hyper"
 static struct signal_struct *zygote32_sig;
 static struct signal_struct *zygote64_sig;
 
@@ -2002,6 +2003,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   SEM_HYPER_BIN_PREFIX,
+					   strlen(SEM_HYPER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
