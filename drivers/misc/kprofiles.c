7e61e08b2190274c5b5acf50d80f404624e8a3c6
Dakkshesh <dakkshesh5@gmail.com>
2021-10-29 16:06:56 +0530

    drivers: kprofiles: Implement msm_drm_notifier for auto kprofiles
    We will be implementing msm drm notifier to enforce battery mode when ever screen is off
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -6,14 +6,47 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kprofiles.h>
+#include <linux/msm_drm_notify.h>
 
-static unsigned int enabled = CONFIG_KPROFILES_MODE;
-module_param(enabled, uint, 0664);
+static bool screen_on = true;
+static unsigned int mode = CONFIG_KPROFILES_MODE;
+module_param(mode, uint, 0664);
 
-unsigned int active_mode(void)
+static int msm_drm_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
 {
-	switch (enabled)
-	{
+	struct msm_drm_notifier *evdata = data;
+	int *blank;
+
+	if (event != MSM_DRM_EVENT_BLANK)
+		goto out;
+
+	if (!evdata || !evdata->data || evdata->id != MSM_DRM_PRIMARY_DISPLAY)
+		goto out;
+
+	blank = evdata->data;
+	switch (*blank) {
+	case MSM_DRM_BLANK_POWERDOWN:
+		if (!screen_on)
+			break;
+		screen_on = false;
+		mode = 1;
+		break;
+	case MSM_DRM_BLANK_UNBLANK:
+		if (screen_on)
+			break;
+		screen_on = true;
+		mode = 0;
+		break;
+	}
+
+out:
+	return NOTIFY_OK;
+}
+
+inline unsigned int active_mode(void)
+{
+	switch (mode) {
 		case 1:
 			return 1;
 			break;
@@ -29,6 +62,19 @@ unsigned int active_mode(void)
 	}
 }
 
+static struct notifier_block fb_notifier_block = {
+	.notifier_call = msm_drm_notifier_callback,
+};
+
+static int  __init kprofiles_notifier_init(void)
+{
+
+	msm_drm_register_client(&fb_notifier_block);
+
+	return 0;
+}
+
+late_initcall(kprofiles_notifier_init);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dakkshesh");
 MODULE_DESCRIPTION("KernelSpace Profiles");
