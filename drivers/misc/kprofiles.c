958ce05fd87ad6d6e92714f17757cfcaf875430f
Dakkshesh <dakkshesh5@gmail.com>
2021-10-29 19:17:15 +0530

    drivers: kprofiles: bump version to 2.0.0
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -111,4 +111,4 @@ late_initcall(kprofiles_notifier_init);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dakkshesh");
 MODULE_DESCRIPTION("KernelSpace Profiles");
-MODULE_VERSION("0.0.1");
+MODULE_VERSION("2.0.0");
