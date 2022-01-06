7e3355d57922f9af6385d486a5c8bf1263196554
repinger <evanf780@gmail.com>
2022-01-06 10:43:35 +0700

    misc: kprofiles: Adapt for FB
    Signed-off-by: ichibauer <evanf780@gmail.com>

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -6,47 +6,16 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/kprofiles.h>
-#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
-#include <linux/msm_drm_notify.h>
-#elif defined(CONFIG_AUTO_KPROFILES_FB)
 #include <linux/fb.h>
-#endif
 
 static bool screen_on = true;
 unsigned int set_mode;
 static unsigned int mode = CONFIG_KPROFILES_MODE;
 module_param(mode, uint, 0664);
 
-static int msm_drm_notifier_callback(struct notifier_block *self,
+static int fb_notifier_callback(struct notifier_block *self,
 				unsigned long event, void *data)
 {
-#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
-	struct msm_drm_notifier *evdata = data;
-	int *blank;
-
-	if (event != MSM_DRM_EVENT_BLANK)
-		goto out;
-
-	if (!evdata || !evdata->data || evdata->id != MSM_DRM_PRIMARY_DISPLAY)
-		goto out;
-
-	blank = evdata->data;
-	switch (*blank) {
-	case MSM_DRM_BLANK_POWERDOWN:
-		if (!screen_on)
-			break;
-		screen_on = false;
-		set_mode = mode;
-		mode = 1;
-		break;
-	case MSM_DRM_BLANK_UNBLANK:
-		if (screen_on)
-			break;
-		screen_on = true;
-		mode = set_mode;
-		break;
-	}
-#elif defined(CONFIG_AUTO_KPROFILES_FB)
 	struct fb_event *evdata = data;
 	int *blank;
 
@@ -55,27 +24,25 @@ static int msm_drm_notifier_callback(struct notifier_block *self,
 
 	blank = evdata->data;
 	switch (*blank) {
-	case FB_BLANK_POWERDOWN:
-		if (!screen_on)
+		case FB_BLANK_POWERDOWN:
+			if (!screen_on)
+				break;
+			screen_on = false;
+			set_mode = mode;
+			mode = 1;
 			break;
-		screen_on = false;
-		set_mode = mode;
-		mode = 1;
-		break;
-	case FB_BLANK_UNBLANK:
-		if (screen_on)
-			break;
-		screen_on = true;
-		mode = set_mode;
-		break;
+		case FB_BLANK_UNBLANK:
+			if (screen_on)
+				break;
+			screen_on = true;
+			mode = set_mode;
 	}
-#endif
 
 out:
 	return NOTIFY_OK;
 }
 
-inline unsigned int active_mode(void)
+unsigned int active_mode(void)
 {
 	switch (mode) {
 		case 1:
@@ -93,18 +60,13 @@ inline unsigned int active_mode(void)
 	}
 }
 
-static struct notifier_block msm_drm_notifier_block = {
-	.notifier_call = msm_drm_notifier_callback,
+static struct notifier_block fb_notifier_block = {
+	.notifier_call = fb_notifier_callback,
 };
 
 static int  __init kprofiles_notifier_init(void)
 {
-
-#ifdef CONFIG_AUTO_KPROFILES_MSM_DRM
-	msm_drm_register_client(&msm_drm_notifier_block);
-#elif defined(CONFIG_AUTO_KPROFILES_FB)
-	fb_register_client(&msm_drm_notifier_block);
-#endif
+	fb_register_client(&fb_notifier_block);
 	return 0;
 }
 
