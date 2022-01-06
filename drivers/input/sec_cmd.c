a163b085caccae63c3ccf4dc0e2d0bd1df4844d7
repinger <evanf780@gmail.com>
2022-01-06 11:06:29 +0700

    input/sec_cmd: Allow build without `SEC_DEBUG_TSP_LOG`
    Signed-off-by: repinger <evanf780@gmail.com>

--- drivers/input/sec_cmd.c
+++ drivers/input/sec_cmd.c
@@ -327,8 +327,9 @@ static void sec_cmd_store_function(struct sec_cmd_data *data)
 		snprintf(tbuf, sizeof(tbuf), "[r:%lu.%06lu]",
 				(unsigned long)t,
 				nanosec_rem / 1000);
-
+#ifdef CONFIG_SEC_DEBUG_TSP_LOG
 		sec_debug_tsp_command_history(tbuf);
+#endif
 	}
 }
 
@@ -373,11 +374,12 @@ static ssize_t sec_cmd_store(struct device *dev, struct device_attribute *devatt
 				snprintf(tbuf, sizeof(tbuf), "[q:%lu.%06lu]",
 						(unsigned long)t,
 						nanosec_rem / 1000);
-
+#if CONFIG_SEC_DEBUG_TSP_LOG
 				snprintf(task_info, 40, "\n[%d:%s]", current->pid, current->comm);
 				sec_debug_tsp_command_history(task_info);
 				sec_debug_tsp_command_history(cmd.cmd);
 				sec_debug_tsp_command_history(tbuf);
+#endif
 
 			}
 			break;
