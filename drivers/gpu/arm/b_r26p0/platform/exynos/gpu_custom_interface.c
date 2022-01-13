b27f68375caf021a5a0c584954dcd9938228be66
Diep Quynh <remilia.1505@gmail.com>
2022-01-13 20:31:21 +0700

    mali: Add sustainable frequency
    Avoid excessive thermal throttling on stock DVFS during gaming.
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: repinger <evanf780@gmail.com>

--- drivers/gpu/arm/b_r26p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/b_r26p0/platform/exynos/gpu_custom_interface.c
@@ -547,6 +547,8 @@ static ssize_t show_max_lock_dvfs(struct device *dev, struct device_attribute *a
 	return ret;
 }
 
+#define SUSTAINABLE_FREQ 546000 // KHz
+
 static ssize_t set_max_lock_dvfs(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
 	int ret, clock = 0;
@@ -567,6 +569,9 @@ static ssize_t set_max_lock_dvfs(struct device *dev, struct device_attribute *at
 			GPU_LOG(DVFS_WARNING, DUMMY, 0u, 0u, "%s: invalid value\n", __func__);
 			return -ENOENT;
 		}
+
+		if (clock < SUSTAINABLE_FREQ)
+			clock = SUSTAINABLE_FREQ;
 
 		platform->user_max_lock_input = clock;
 
